fec15b62e0979d421c5aa80e276fda6b4c697d67
Murillo Oliveira <murillo1047@gmail.com>
2023-07-15 21:45:35 -0300

    commit

---
+++ cursoReact/projeto1/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    posts: [
+      
+    ]
+  };
+
+  render(){
+    const { name, counter} = this.state;
+
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
