adaff82212d6335c439f31f8a81e9c3f0d428644
Murillo Oliveira <murillo1047@gmail.com>
2023-07-24 19:39:14 -0300

    feat: projeto1

--- cursoReact/projeto1/src/components/button/index.jsx
+++ cursoReact/projeto1/src/components/button/index.jsx
@@ -1,24 +1,18 @@
-import './styles.css';
 import P from 'prop-types';
-import { Component } from 'react';
+import './styles.css';
 
-export class Button extends Component{
-    render(){
-        const {text, onClick, disabled = false} = this.props;
-        return(
-            <button disabled={disabled} className='button' onClick={onClick}>
-                {text}
-            </button>
-        )
-    }
-}
+export const Button = ({ text, onClick, disabled = false }) => (
+  <button className="button" onClick={onClick} disabled={disabled}>
+    {text}
+  </button>
+);
 
 Button.defaultProps = {
-  disabled: false
-}
+  disabled: false,
+};
 
 Button.propTypes = {
   text: P.string.isRequired,
-  onClick: P.string.isRequired,
+  onClick: P.func.isRequired,
   disabled: P.bool,
-}
+};
