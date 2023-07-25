b60f3377f045872fec9efd95949b302ac9aa0071
Murillo Oliveira <murillo1047@gmail.com>
2023-07-25 11:47:00 -0300

    useMemo

---
+++ cursoReact/projeto1/src/App.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+      return (
+        <div>
+          <h1>Olá mundo</h1>
+        </div>
+      );
+    }
+  }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
